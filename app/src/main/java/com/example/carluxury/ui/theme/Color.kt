8239c5c852a0206716c4d7039c0b6e6a88142ccd
jonathan kalonga <jokalmwamba@gmail.com>
2023-04-09 13:38:12 +0200

    add color for light mode

--- app/src/main/java/com/example/carluxury/ui/theme/Color.kt
+++ app/src/main/java/com/example/carluxury/ui/theme/Color.kt
@@ -2,6 +2,18 @@ package com.example.carluxury.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+//light color
+val orange1 = Color(0xFFF9B398)
+val orange2 = Color(0xFFFBD1C1)
+val orange3 = Color(0xFFD0886E)
+val mallow1 = Color(0xFF4B4960)
+val mallow2 = Color(0xFF3F3E55)
+val white = Color(0xFFFFFFFF)
+val grey1 = Color(0xFF393939)
+val grey2 = Color(0xFF888888)
+val green1 = Color(0xFF5DA635)
+
+
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
